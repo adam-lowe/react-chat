eba0ff4fef23477b911b2921b97eb5619a8adb5c
Goldpm <lowedavidadam@gmail.com>
2020-01-31 14:49:57 -0500

    added dummy data to new format

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import { useState } from 'react';
 import './App.css';
 import Title from './Components/Title';
 import MessageHistory from './Components/MessageHistory';
 import MessageInput from './Components/MessageInput';
 
+const DUMMY_DATA = [
+  {
+    senderId: "perborgen",
+    text: "who'll win?"
+  },
+  {
+    senderId: "janedoe",
+    text: "who'll win?"
+  }
+]
+
 function App() {
+  const [messageState, setMessageState] = useState({
+    messages: DUMMY_DATA
+  });
+
   return ( 
     <div className="app">
         <Title />
-        <MessageHistory messages={this.state.messages} />
+        <MessageHistory messages={messageState.messages} />
         <MessageInput />
     </div>
  );
