d51fa9e673afbaa2df19e07e1afdbeaf3299fc8d
Goldpm <lowedavidadam@gmail.com>
2020-01-31 13:43:59 -0500

    added dummy data for message history

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
+const DUMMY_DATA = [
+    {
+      senderId: "perborgen",
+      text: "who'll win?"
+    },
+    {
+      senderId: "janedoe",
+      text: "who'll win?"
+    }
+  ]
 class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            messages: DUMMY_DATA
+        }
+    }
     render() { 
         return ( 
             <div className="app">
                 <Title />
-                <MessageHistory />
+                <MessageHistory messages={this.state.messages} />
                 <MessageInput />
             </div>
          );
