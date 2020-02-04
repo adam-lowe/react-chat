a02ffe5fe516485c0717e1d15acb8ab6636cf62f
Goldpm <lowedavidadam@gmail.com>
2020-02-04 01:46:04 -0500

    removed roomID

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ const tokenProvider = new TokenProvider({
 });
 const instanceLocator = "v1:us1:800d59fe-fa6a-496b-92da-d13fd0fb3ad6"
 const userId = "Gabachi"
-const roomId = "f9ef568c-5b8a-4ba6-a35b-e90503b9c1cc"
 
 class App extends Component {
   state = { messages: []}
