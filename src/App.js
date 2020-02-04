16bc8da4bbee081c593e613ee0dec83f5d5df6f5
Goldpm <lowedavidadam@gmail.com>
2020-02-03 23:40:26 -0500

    added chatkit info

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import './App.css';
 // import Title from './Components/Title';
 import MessageHistory from './Components/MessageHistory';
 // import MessageInput from './Components/MessageInput';
+const instanceLocator = "v1:us1:800d59fe-fa6a-496b-92da-d13fd0fb3ad6"
+const testToken = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/800d59fe-fa6a-496b-92da-d13fd0fb3ad6/token"
+const username = "Gabachi"
+const roomId = "f9ef568c-5b8a-4ba6-a35b-e90503b9c1cc"
 
 const DUMMY_DATA = [
   {
