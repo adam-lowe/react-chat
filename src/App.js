cbc5d174f93f8a052cf7ae7332de3ac331b82510
Goldpm <lowedavidadam@gmail.com>
2020-02-04 01:45:01 -0500

    removed dummy data

--- src/App.js
+++ src/App.js
@@ -17,17 +17,6 @@ const instanceLocator = "v1:us1:800d59fe-fa6a-496b-92da-d13fd0fb3ad6"
 const userId = "Gabachi"
 const roomId = "f9ef568c-5b8a-4ba6-a35b-e90503b9c1cc"
 
-const DUMMY_DATA = [
-  {
-    senderId: "perborgen",
-    text: "who'll win?"
-  },
-  {
-    senderId: "janedoe",
-    text: "I'll win!"
-  }
-]
-
 class App extends Component {
   state = { messages: []}
 
