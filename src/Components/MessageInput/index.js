0debb011857ea3c483c075d919cca49b8de0dad8
Goldpm <lowedavidadam@gmail.com>
2020-02-07 10:39:37 -0500

    added state and basic handle functions to MessageInput

--- src/Components/MessageInput/index.js
+++ src/Components/MessageInput/index.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Component } from 'react';
 
-function MessageInput(props) {
-      return (
-        <div></div>
-      )
+class MessageInput extends Component {
+    state = {
+        message: ''
     }
+
+    handleChange(e) {
+        this.setState({
+            message: e.target.value
+        })
+    }
+    
+    handleSubmit(e) {
+        e.preventDefault()
+        this.props.sendMessage(this.state.message)
+        this.setState({
+            message: ''
+        })
+    }
+  render() {
+    return ( 
+      <div className="MessageInput">
+      </div>
+   );
+    }
+  }
 
   export default MessageInput;
