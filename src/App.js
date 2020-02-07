6b48e87c1c4a93fcc29d6f71b4b6998b2000352b
Goldpm <lowedavidadam@gmail.com>
2020-02-07 10:08:59 -0500

    too many issues with auth, dropping chatkit api

--- src/App.js
+++ src/App.js
@@ -1,31 +1,9 @@
 import React from 'react';
 import { Component } from 'react';
-import {
-  ChatkitProvider,
-  TokenProvider,
-  withChatkit,
-} from "@pusher/chatkit-client-react"
-import axios from 'axios';
 import './App.css';
 // import Title from './Components/Title';
 import MessageList from './Components/MessageList';
 // import MessageInput from './Components/MessageInput';
-
-const tokenProvider = new TokenProvider({
-  url: "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/800d59fe-fa6a-496b-92da-d13fd0fb3ad6/token",
-});
-const instanceLocator = "v1:us1:800d59fe-fa6a-496b-92da-d13fd0fb3ad6"
-const instanceId = "800d59fe-fa6a-496b-92da-d13fd0fb3ad6"
-const userId = "Gabachi"
-const roomId = "f9ef568c-5b8a-4ba6-a35b-e90503b9c1cc"
-
-axios({
-  method:'get',
-  url: `https://us1.pusherplatform.io/services/chatkit/v6/${instanceId}/rooms/${roomId}/messages`
-})  
-  .then(function (response) {
-    console.log(response.data)
-});
 
 
 class App extends Component {
@@ -34,14 +12,9 @@ class App extends Component {
 render() {
   return ( 
     <div className="app">
-          <ChatkitProvider
-    instanceLocator={instanceLocator}
-    tokenProvider={tokenProvider}
-    userId={userId}>
         {/* <Title /> */}
         <MessageList messages={this.state.messages} />
         {/* <MessageInput /> */}
-    </ChatkitProvider>
     </div>
  );
   }
