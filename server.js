2ee8fbde5297c4fdd501528b65a61f9a640f0003
Goldpm <lowedavidadam@gmail.com>
2020-02-10 15:09:16 -0500

    integrated morgan

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const logger = require("morgan");
 
 const app = express()
+
+app.use(logger("dev"));
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
+
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+});
 
 const PORT = 3001
 app.listen(PORT, err => {
