11b9673fed5954af561f3f2793faf4a8c61802c2
Goldpm <lowedavidadam@gmail.com>
2020-02-11 22:41:15 -0500

    Added title component code

---
+++ src/Components/Title/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Component } from 'react';
+
+class Title extends Component {
+  render() {
+    return ( 
+      <div className="Title">
+       <h1>React-Chat</h1>
+      </div>
+   );
+    }
+  }
+
+  export default Title;
