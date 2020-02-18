be6ae32332503444feff4bbe2da6bbca8b7aa9fc
Goldpm <lowedavidadam@gmail.com>
2020-02-18 18:44:45 -0500

    formatted

--- src/Components/MessageInput/index.js
+++ src/Components/MessageInput/index.js
@@ -1,33 +1,38 @@
-import React from 'react';
-import { Component } from 'react';
+import React from "react";
+import { Component } from "react";
 
 class MessageInput extends Component {
-    state = {
-        message: ''
-    }
+  state = {
+    message: ""
+  };
 
-    handleChange(e) {
-        this.setState({
-            message: e.target.value
-        })
-    }
-    
-    handleSubmit(e) {
-        e.preventDefault()
-        this.props.sendMessage(this.state.message)
-        this.setState({
-            message: ''
-        })
-    }
-  render() {
-    return ( 
-      <div className="MessageInput">
-        <form className="send-message-form">
-            <input type="text" name="pendingMessage" id="messageInput" placeholder="Type your message and press ENTER" />
-        </form>
-      </div>
-   );
-    }
+  handleChange(e) {
+    this.setState({
+      message: e.target.value
+    });
   }
 
-  export default MessageInput;
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.sendMessage(this.state.message);
+    this.setState({
+      message: ""
+    });
+  }
+  render() {
+    return (
+      <div className="MessageInput">
+        <form className="send-message-form">
+          <input
+            type="text"
+            name="pendingMessage"
+            id="messageInput"
+            placeholder="Type your message and press ENTER"
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default MessageInput;
